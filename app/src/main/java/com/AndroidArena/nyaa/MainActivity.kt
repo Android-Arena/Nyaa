1778a87d273ac26bf07a730ba0f3b925e62caef1
paulr <paulr756756@gmail.com>
2021-11-16 16:48:45 +0530

    frame 0

--- app/src/main/java/com/AndroidArena/nyaa/MainActivity.kt
+++ app/src/main/java/com/AndroidArena/nyaa/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.AndroidArena.nyaa
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.SemanticsActions.OnClick
 import androidx.compose.ui.tooling.preview.Preview
 import com.AndroidArena.nyaa.ui.theme.NyaaTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import com.AndroidArena.nyaa.ui.theme.primaryColor
+import com.AndroidArena.nyaa.ui.theme.primaryTextColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
             NyaaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    frame1()
                 }
             }
         }
@@ -31,9 +34,57 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun frame1(){
-    Column(modifier= Modifier .weight(1f)){
-        OutlinedButton(onClick = { /*TODO*/ }) {
-            Text(text = "Sign Up")
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 40.dp)
+
+    ) {
+        Column( modifier = Modifier
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+
+
+        ) {
+            Text(text = "Welcome to chatting app for weebs")
+            Button(
+                onClick = { /*TODO*/ },
+                modifier=Modifier
+                    .size(width=361.dp, height =56.dp)
+                    .padding(vertical = 135.dp),
+                contentPadding = PaddingValues(16.dp),
+                shape= CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = primaryColor,
+                    contentColor = primaryTextColor,
+                ),
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 4.dp,
+                    pressedElevation = 7.dp,
+                ),
+            ){
+                Text(text = "Sign Up")
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier= Modifier
+                    .size(width=361.dp, height =56.dp)
+                    .padding(vertical = 38.dp),
+
+                contentPadding = PaddingValues(16.dp),
+                shape= CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = primaryTextColor,
+                    contentColor = primaryColor,
+                ),
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 4.dp,
+                    pressedElevation = 7.dp,
+                ),
+
+                ) {
+                Text(text = "Login")
+            }
         }
     }
 }
@@ -49,6 +100,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     NyaaTheme {
-        Greeting("Android")
+        frame1()
     }
 }
