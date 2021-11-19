78b251786d935ffcdf0c6442e94e35bc67b3b8cb
Vaibhav Goel <goelvh@gmail.com>
2021-11-19 18:13:50 +0530

    deleting

--- app/src/main/java/com/AndroidArena/nyaa/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.AndroidArena.nyaa
-
-sealed class Screen(val route:String){
-    object splashScreen :Screen("splash_screen")
-    object frame1 : Screen("frame_1")
-    object frame2 : Screen("frame_2")
-}
